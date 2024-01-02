d23a0479245832ad0415d84016b750f879bb2a66
Anjali <anjalimane424@gmail.com>
2024-01-02 11:36:18 +0530

    main.java updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("My name is "+ sc.next());
+        int a = 123_000_000;
+        byte b = (byte)a;
+        System.out.println("byte b = "+b);
+        float tempF, tempC;
+        System.out.println("Temperature in Celsius is ");
+        tempC = sc.nextFloat();
+        tempF = (tempC *9/5) +32;
+        System.out.println("Temperature in Fahrenheit is " + tempF);
+
     }
 }
