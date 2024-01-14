35e4c015e1049bd11a19c7e8cd40f7a09da15836
Mane <anjali.shamsundar-mane@capgemini.com>
2024-01-14 13:10:41 +0530

    Just some array practice

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,15 +6,33 @@ public class Main {
 
         System.out.println("Hello!");
         Scanner sc = new Scanner(System.in);
-        System.out.println("My name is "+ sc.next());
-        int a = 123_000_000;
-        byte b = (byte)a;
-        System.out.println("byte b = "+b);
-        float tempF, tempC;
-        System.out.println("Temperature in Celsius is ");
-        tempC = sc.nextFloat();
-        tempF = (tempC *9/5) +32;
-        System.out.println("Temperature in Fahrenheit is " + tempF);
+//        System.out.println("My name is "+ sc.next());
+//        int a = 123_000_000;
+//        byte b = (byte)a;
+//        System.out.println("byte b = "+b);
+//        float tempF, tempC;
+//        System.out.println("Temperature in Celsius is ");
+//        tempC = sc.nextFloat();
+//        tempF = (tempC *9/5) +32;
+//        System.out.println("Temperature in Fahrenheit is " + tempF);
+
+        int [][] arr= new int[3][2];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j <arr[i].length ; j++) {
+                arr[i][j]=sc.nextInt();
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+        for (int i[]: arr)
+        {
+            System.out.println(Arrays.toString(i));
+        }
+
 
 
     }
